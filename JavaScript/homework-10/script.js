2b390fc0e46b900a0befbc500de2324710bed039
Unknown <andy.podust@me.com>
2018-10-18 07:58:10 +0300

    llk

--- JavaScript/homework-10/script.js
+++ JavaScript/homework-10/script.js
@@ -17,22 +17,23 @@ removeUser.addEventListener("submit", hamdleRemoveUserBtn);
 addUser.addEventListener("submit", handleAddUserBtn);
 updateUser.addEventListener("submit", handleUdateUserBtn);
 
-function fetchUserData(id, param) {
-  return fetch(`https://test-users-api.herokuapp.com/users/${id}`, param)
-  .then(response => {
-      if(response.ok) return response.json();
-      throw new Error('error')
-  })
-  .catch(error => console.error(error));
-};
+function fetchUserData(id, param){
 
-function handleAllUserBtn(ev) {
-  ev.preventDefault();
+  return fetch(`https://test-users-api.herokuapp.com/users/${id}`, param)
+       .then(response => {
+           if(response.ok) return response.json();
+
+           throw new Error('error')
+       }).catch(err => console.error(err));
+}
+
+function handleAllUsersBtn(evt) {
+  evt.preventDefault();
   fetchUserData('').then(data => {
-    const names = data.data.reduce((acc, el) => acc + `<tr> <td>${el.id}</td>    <td>${el.name}</td>  <td>${el.age}</td> </tr>`,'');
-    result.firstElementChild.innerHTML = `<caption>All users</caption><tr><td>ID</td><td>NAME</td><td>AGE</td></tr>${names}`;
-});
-};
+      const names = data.data.reduce((acc, el) => acc + `<tr> <td>${el.id}</td>    <td>${el.name}</td>  <td>${el.age}</td> </tr>`,'');
+      result.firstElementChild.innerHTML = `<caption>All users</caption><tr><td>ID</td><td>NAME</td><td>AGE</td></tr>${names}`;
+  });
+} 
 
 function handleGetUsersByIdBtn(ev) {
   ev.preventDefault();
