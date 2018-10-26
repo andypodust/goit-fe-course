a13a898fab5401ef7e17110756011b4aaef4711a
Unknown <andy.podust@me.com>
2018-10-26 09:33:03 +0300

    HW-12

---
+++ JavaScript/homework-12/build/js/scripts.js
@@ -0,0 +1,54 @@
+"use strict";
+
+var refs = {
+  template: document.querySelector('.js-template'),
+  listCards: document.querySelector('.js-list-cards'),
+  input: document.querySelector('.js-form input[type=text]'),
+  form: document.querySelector('.js-form')
+};
+var source = refs.template.innerHTML.trim();
+var temp = Handlebars.compile(source);
+Object.keys(localStorage).forEach(function (element) {
+  fetch("http://api.linkpreview.net/?key=5bb3d3d85d2689e5629a2de473cd5362919f9edc33853&q=".concat(element)).then(function (response) {
+    if (response.ok) return response.json();
+  }).then(function (data) {
+    refs.listCards.insertAdjacentHTML('afterbegin', temp(data));
+  }).catch(function (err) {
+    return console.error(err);
+  });
+});
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  var regUrl = /^((ftp|http|https):\/\/)?(www\.|)([A-z|0-9]+)\.([A-z]{2,})/;
+
+  if (!regUrl.test(refs.input.value)) {
+    refs.input.parentNode.reset();
+    return alert('Невалидный URL! Попробуйте еще раз.');
+  }
+
+  fetch("http://api.linkpreview.net/?key=5bb3d3d85d2689e5629a2de473cd5362919f9edc33853&q=".concat(refs.input.value)).then(function (response) {
+    if (response.ok) return response.json();
+  }).then(function (data) {
+    if (localStorage.getItem(data.url) !== null) return alert('Такая закладка уже есть!');
+    localStorage.setItem(data.url, data.url);
+    var markup = temp(data);
+    refs.listCards.insertAdjacentHTML('afterbegin', markup);
+  }).catch(function (err) {
+    return console.error(err);
+  });
+  ;
+  ;
+  refs.input.parentNode.reset();
+}
+
+function handleDelBtn(e) {
+  e.preventDefault();
+  var liUrl = document.querySelector('.js-li-url');
+  if (e.target.nodeName !== "BUTTON") return;
+  localStorage.removeItem(liUrl.name);
+  e.target.parentElement.parentElement.remove();
+}
+
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.listCards.addEventListener('click', handleDelBtn);
---
+++ JavaScript/homework-12/src/js/link-list.js
@@ -0,0 +1,64 @@
+const refs = {
+     template: document.querySelector('.js-template'),
+     listCards: document.querySelector('.js-list-cards'),
+     input: document.querySelector('.js-form input[type=text]'),
+     form: document.querySelector('.js-form')
+
+};
+
+const source = refs.template.innerHTML.trim();
+const temp = Handlebars.compile(source);
+
+
+Object.keys(localStorage).forEach(element => {
+    fetch(`http://api.linkpreview.net/?key=5bb3d3d85d2689e5629a2de473cd5362919f9edc33853&q=${element}`)
+	.then(response => {
+		if(response.ok) return response.json();
+    })
+    .then(data => {
+		refs.listCards.insertAdjacentHTML('afterbegin', temp(data));
+    })
+    .catch(err => console.error(err));
+});
+
+
+function handleFormSubmit(evt) {
+	evt.preventDefault();
+    
+    const regUrl = /^((ftp|http|https):\/\/)?(www\.|)([A-z|0-9]+)\.([A-z]{2,})/;
+
+	if(!regUrl.test(refs.input.value)) {
+		refs.input.parentNode.reset();
+		return alert('Невалидный URL! Попробуйте еще раз.');
+	} 
+
+    
+    fetch(`http://api.linkpreview.net/?key=5bb3d3d85d2689e5629a2de473cd5362919f9edc33853&q=${refs.input.value}`)
+	.then(response => {
+		if(response.ok) return response.json();
+    })
+    .then(data => {
+		if(localStorage.getItem(data.url) !== null) return alert('Такая закладка уже есть!')
+		localStorage.setItem(data.url, data.url)
+		const markup = temp(data)
+		refs.listCards.insertAdjacentHTML('afterbegin', markup);
+    })
+    .catch(err => console.error(err));;	;
+
+	refs.input.parentNode.reset();
+}
+
+function handleDelBtn(e) {
+	e.preventDefault();
+
+	const liUrl = document.querySelector('.js-li-url');
+
+	if (e.target.nodeName !== "BUTTON") return;
+
+	localStorage.removeItem(liUrl.name);
+
+	e.target.parentElement.parentElement.remove();	
+}
+
+refs.form.addEventListener('submit', handleFormSubmit);
+refs.listCards.addEventListener('click', handleDelBtn)
